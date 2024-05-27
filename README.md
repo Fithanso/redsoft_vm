e3efedad00b2dba049d6575a36cf844cf763f7ea
Matvey <fms160602@gmail.com>
2024-05-27 05:53:55 +0300

    Created a deployment instruction in README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # redsoft_vm
+
+## Deployment
+
+For application to work you need to:
+1. Have `.env` file in the project root with following variables:
+```
+DB_PASSWORD=[db password]
+ENCRYPTION_SALT=[salt must be 16 characters long]
+ENCRYPTION_KEY=[key must be 16, 24, or 32 characters long]
+```
+
+
+2. Install poetry
+3. Run `poetry install` from project root
+4. Enter she with `poetry shell`
+5. You can now run `server.py` and `client.py`
--- settings.py
+++ settings.py
@@ -18,9 +18,9 @@ ENCRYPTION_SALT = os.environ.get('ENCRYPTION_SALT')
 ENCRYPTION_KEY = os.environ.get('ENCRYPTION_KEY')
 
 # test strings
-# add_vm ram_amount:16 dedicated_cpu:4 host:127.0.3.9 port:8000 login:fithanso password:passwrd
-# connect_to_vm 127.0.3.9 8000
-# logout 127.0.0.7 8005
+# add_vm ram_amount:16 dedicated_cpu:4 host:127.0.4.9 port:8000 login:fithanso password:passwrd
+# connect_to_vm 127.0.0.7 8005
+# logout 127.0.4.9 8000
 # disconnect 127.0.0.7 8005
-# update_vm 127.0.0.7 8005 ram_amount:3 dedicated_cpu:5
+# update_vm 127.0.4.9 8000 ram_amount:3 dedicated_cpu:10
 # show_hard_drives
